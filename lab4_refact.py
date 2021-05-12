7c9bd8a0449eac8b36cdc7c2035c5154e01e8e3a
Winter4 <Crowl1ngNice@yandex.ru>
2021-05-12 18:44:19 +0300

    TODO: diff func

--- lab4_refact.py
+++ lab4_refact.py
@@ -1,7 +1,13 @@
 import math
 
 def function(x_required):
-    return math.e  ** math.sin(x_required)
+    return math.exp(math.sin(x_required))
+
+def d1(x_required):
+    return math.exp(math.sin(x_required) * math.cos(x_required))
+
+def d2(x_required):
+    return (-math.sin(x) + (math.cos(x_required))**2) * math.exp(math.sin(x_required))
 
 def print_diffs(diffs):
     # шапка таблицы разностей
@@ -59,9 +65,30 @@ def differentiate_second(x_required, step, x_source, y_source, diffs):
                     bracket_sum2 += bracket_product
             
             bracket_sum1 += bracket_sum2
-        result += diffs[i][0] / factorial 
-    return result / step**2
+        result += diffs[i][0] / factorial * bracket_sum1
+    return -result / step**2
 
+def differentiate_first_manual(x_required, step, x_min, diffs):
+    t = (x_required - x_min) / step
+    
+    tmp = []
+    tmp.append(diffs[0][0])
+    tmp.append((2*t - 1) / 2 * diffs[1][0])
+    tmp.append((3*t**2 - 6*t**2 + 2) / 6 * diffs[2][0])
+    tmp.append((4*t**3 - 18*t**2 + 22*t - 6) / 24 * diffs[3][0])
+    #tmp.append(())
+    return sum(tmp) / step
+
+def differentiate_second_manual(x_required, step, x_min, diffs):
+    t = (x_required - x_min) / step
+    
+    tmp = []
+    tmp.append(diffs[1][0])
+    tmp.append((t - 1)*diffs[2][0])
+    tmp.append((6*t**2 - 18*t + 11) / 12 * diffs[3][0])
+    
+    return -sum(tmp) / step**2
+    
     # main
     
 x_source = [] # список аргументов
@@ -108,10 +135,15 @@ for diff_len in range(values_number - 2, 0, -1):
             
 print_diffs(diffs)
 
-#print("  X", "d1", "d2", sep = 10 * " ")
+print("  X", "d1 N", "d1 M", "d2 N", "d2 M", sep = 8 * " ")
 args = [-0.18, 0.18, -0.45, 0.45, 0.775, 1.225, 0.55, 1.45]
-print (differentiate_first(0.18, step, x_source, y_source, diffs))
-print (differentiate_second(0.18, step, x_source, y_source, diffs))
+#print (differentiate_first(0.18, step, x_source, y_source, diffs))
+#print (differentiate_second(0.18, step, x_source, y_source, diffs))
+#print()
+#print(differentiate_first_manual(0.18, step, x_source[0], diffs))
+#print(differentiate_second_manual(0.18, step, x_source[0], diffs))
 
-#for x in args:
-   # print("%6.2f" % x, "%11.6f" % differentiate_first(x, step, x_source, y_source, diffs))
+for x in args:
+    #print("%6.2f" % x, "%11.6f" % differentiate_first(x, step, x_source, y_source, diffs), "%11.6f" % differentiate_second(x, step, x_source, y_source, diffs), "%11.6f" % differentiate_first_manual(x, step, x_source[0], diffs), "%11.6f" % differentiate_second_manual(x, step, x_source[0], diffs))
+
+    print("%6.2f" % x, "%11.6f" % differentiate_first(x, step, x_source, y_source, diffs), "%11.6f" % d1(x), "%11.6f" % differentiate_second(x, step, x_source, y_source, diffs), "%11.6f" % d2(x))
