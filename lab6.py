21c97e192960dee1b60e29e6ea87612f8a90c911
Winter4 <Crowl1ngNice@yandex.ru>
2021-06-01 11:58:50 +0300

    Passed

--- lab6.py
+++ lab6.py
@@ -11,15 +11,19 @@ d = 10 # до
 n = 30 # кол-во итераций
 h = (d - c) / n # шаг
 
-# отрезок локализации
-a = -1.5
-b = 1.5
-
 eps = 0.001 # точность
 
-print("   i ", "  left  ", " right", "  P  ", " middle  ")
+print("   i ", "  from  ", "  to   ", "  P  ", " middle  ")
 for i in range(n + 1):
     p = c + i * h
+    
+    # отрезок локализации
+    x = 0.1
+    step = 0.1
+    while (f(x, p) * f(x + step, p) > 0):
+        x += step
+    a = x
+    b = x + step
     
     left = a
     right = b
@@ -37,9 +41,9 @@ for i in range(n + 1):
         length = right - left
         ff = f(left, p) * f(middle, p)
         
-    print(str(i + 1).rjust(4), " | ", "%.4f" % left, "  ", "%.4f" % right, "  ", "%.1f" % p, "  ", "%.6f" % middle, sep = "")
+    print(str(i + 1).rjust(4), " | ", "%.4f" % a, "  ", "%.4f" % b, "  ", "%.1f" % p, "  ", "%.6f" % middle, sep = "")
 
-x0 = [x * 0.1 for x in range(-10,10,2)]
+x0 = [x * 0.01 for x in range(-30, 100, 1)]
 y1 = list()
 for x in x0:
     y1.append(f(x, c))
@@ -47,8 +51,10 @@ for x in x0:
 y2 = list()
 for x in x0:
     y2.append(f(x, d))
-    
+
 plt.plot(x0, y1, label="F(x, c)")
 plt.plot(x0, y2, label="F(x, d)")
 plt.legend(loc="best")
+plt.axhline(0, linewidth=0.8, color="black")
+plt.axvline(0, linewidth=0.8, color="black")
 plt.show()
